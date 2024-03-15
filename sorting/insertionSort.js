00ad9ae422a0a9d4c22f62d819444b7d8c4098f0
kamboj7791 <kambojkunal01@gmail.com>
2024-03-16 01:22:58 +0530

    insetion sort

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,7 +1,16 @@
 //given an array of integers , sort the array.
-function insertionSort(arr) {}
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let numberToInserted = arr[i];
+    let j = i - 1;
+    while (j <= 0 && j > numberToInserted) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = numberToInserted;
+  }
+}
 
 let arr = [-2, 3, -4, 8, -9, 10];
 insertionSort(arr);
-console.log(arr);
 console.log(arr);
