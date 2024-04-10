2fba3de151d154506b8ccdd7c6cf46a9219b81f1
Kamboj7791 <91943520+Kamboj7791@users.noreply.github.com>
2024-04-10 22:55:52 +0530

    Update bubbleSort.js

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -5,7 +5,7 @@ function bubbleSort(array) {
     swapped = false;
     for (let i = 0; i < array.length - 1; i++) {
       if (array[i] > array[i + 1]) {
-        // console.log(swapped);
+        console.log(swapped);
         let temp = array[i];
         array[i] = array[i + 1];
         array[i + 1] = temp;
